a131ed5cea45fa7208b1b9897a2da64f3e2a3dd4
ddonvan <donovand9@mymacewan.ca>
2024-04-01 22:13:16 -0600

    results screen implemented

--- hyperpopGG/Assets/Scripts/GameManager.cs
+++ hyperpopGG/Assets/Scripts/GameManager.cs
@@ -23,6 +23,15 @@ public class GameManager : MonoBehaviour
     public Text scoreText;
     public Text multiText;
 
+    public float totalNotes;
+    public float normalHits;
+    public float goodHits;
+    public float perfectHits;
+    public float missedHits;
+
+    public GameObject resultsScreen;
+    public Text percentHitText, normalsText, goodsText, perfectsText, missesText, rankText, finalScoreText;
+
 
 
     // Start is called before the first frame update
@@ -32,6 +41,8 @@ public class GameManager : MonoBehaviour
 
         scoreText.text = "Score: 0";
         currentMultiplier = 1;
+
+        totalNotes = FindObjectsOfType<NoteObject>().Length;
     }
 
     // Update is called once per frame
@@ -45,6 +56,49 @@ public class GameManager : MonoBehaviour
                 theBS.hasStarted = true;
 
                 theMusic.Play();
+            }
+        } else
+        {
+            if(!theMusic.isPlaying && !resultsScreen.activeInHierarchy)
+            {
+                resultsScreen.SetActive(true);
+
+                normalsText.text = normalHits.ToString();
+                goodsText.text = goodHits.ToString();
+                perfectsText.text = perfectHits.ToString();
+                missesText.text = missedHits.ToString();
+
+                float totalHit = normalHits + goodHits + perfectHits;
+                float percentHit = (totalHit/totalNotes) * 100f;
+
+                percentHitText.text = percentHit.ToString("F1") + "%";
+
+                string rankVal = "F";
+
+                if(percentHit > 40)
+                {
+                    rankVal = "D";
+                    if(percentHit > 55)
+                    {
+                        rankVal = "C";
+                        if(percentHit > 70)
+                        {
+                            rankVal = "B";
+                            if(percentHit > 85)
+                            {
+                                rankVal = "A";
+                                if(percentHit > 95)
+                                {
+                                    rankVal = "S";
+                                }
+                            }
+                        }
+                    }
+                }
+
+                rankText.text = rankVal;
+
+                finalScoreText.text = currentScore.ToString();
             }
         }
     }
@@ -76,18 +130,23 @@ public class GameManager : MonoBehaviour
     {
         currentScore += scorePerNote * currentMultiplier;
         NoteHit();
+
+        normalHits++;
     }
 
     public void GoodHit()
     {
         currentScore += scorePerGoodNote * currentMultiplier;        
         NoteHit();
+
+        goodHits++;
     }
 
     public void PerfectHit()
     {
         currentScore += scorePerPerfectNote * currentMultiplier;
         NoteHit();
+        perfectHits++;
     }
 
     public void NoteMissed()
@@ -98,5 +157,7 @@ public class GameManager : MonoBehaviour
         multiplierTracker = 0;
 
         multiText.text = "Multiplier: x" + currentMultiplier;
+
+        missedHits++;
     }
 }
